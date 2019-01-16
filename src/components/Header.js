d744976512bcf44d102197a4e33be64c20f0f4b8
Kye Berkeland <kyeberkeland@yahoo.com>
2019-01-16 14:49:20 -0600

    Add Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className="Header">
+            <h1>React Props Activity</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
